32388e95516198f0ecf5699b5577dc6041f3abc0
Johan Champlon <johanchamplon@sfr.fr>
2019-08-29 11:23:41 +0200

    ajout iconCart et cartDropdown et ajout cartReducer et fonction hidden

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { ToggleCartHidden } from '../../redux/cart/cart.actions';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+
+import './cart-icon.styles.scss';
+
+const CartICon = ({ ToggleCartHidden }) => (
+    <div className='cart-icon' onClick={ToggleCartHidden}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>0</span>
+    </div>
+)
+
+const mapDispatchToProps = dispatch => ({
+    ToggleCartHidden: () => dispatch(ToggleCartHidden())
+})
+
+
+export default connect(
+    null,
+    mapDispatchToProps)(CartICon);

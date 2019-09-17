e2c79e570c4ec5ff783445f664b62ea57c97a325
Johan Champlon <johanchamplon@sfr.fr>
2019-09-17 15:03:47 +0200

    ajout d'une HOC + remis css sur custom-buttom

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,10 +1,9 @@
-import SHOP_DATA from './shop.data';
-
 import ShopActionTypes from './shop.types';
+import SHOP_DATA from './shop.data';
 
 const INITIAL_STATE = {
     collections: SHOP_DATA
-}
+};
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
